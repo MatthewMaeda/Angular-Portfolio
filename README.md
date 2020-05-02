3dbe2644b25bc3621961644694d5677cdb2950c7
MatthewMaeda <maedam@mail.gvsu.edu>
2020-05-01 22:45:52 -0400

    Pushing to github

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularPortfolio
+# AngularPortfolio by Matthew Maeda
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.22.
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
